ca99109d77a2fdb5ca62d4016cfbfbf083c5b84e
ddanny16 <dammydan16@gmail.com>
2022-05-05 16:58:17 +0200

    ...

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const menuBtn = document.querySelector('.menu-btn')
 const navigation = document.querySelector('.navigation')
 const fas = document.querySelector('.fas')
-
+const blurOverlay =document.querySelector('.blur_overlay')
 const IS_ACTIVE = 'is--active'
+
 
 const toggleNavigation = () =>{
     navigation.classList.toggle(IS_ACTIVE)
+    blurOverlay.classList.toggle(IS_ACTIVE)
 }
 
 const click ='click'
 
 menuBtn.addEventListener(click , toggleNavigation)
 fas.addEventListener(click , toggleNavigation)
+blurOverlay.addEventListener(click , toggleNavigation)
